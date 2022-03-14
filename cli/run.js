f8eb39e8ef2ec688be7c5a4dbc3a286282a7eac8
Alexander Günsche <git@lxg.de>
2022-03-14 11:54:58 +0100

    update module resolution

--- cli/run.js
+++ cli/run.js
@@ -15,7 +15,7 @@
 import * as fs from 'fs'
 import path from 'path'
 import fetch from 'node-fetch'
-import getManifest from 'openmfe-manifest'
+import getManifest from '@openmfe/manifest'
 
 // checks
 import Environment from '../lib/environment.js'
@@ -28,7 +28,7 @@ import checkPrerenderValid from '../lib/valid-prerender.js'
     if (!manifestUrl)
         throw new Error("An URL to the manifest file must be provided.")
 
-    const manifest = getManifest(await fetch(manifestUrl).then(data => data.text()), manifestUrl)
+    const manifest = await getManifest(manifestUrl)
 
     const env = new Environment()
     await env.init()
